072fbfd37078df5b1a2c5580747c1f0c44bff203
tilei.li <tellhelloworld@gmail.com>
2021-10-19 13:20:47 +0800

    +editor script

---
+++ Assets/Noitom/PerceptionNeuron/Scripts/Editor/NeuronTrackerInstanceEditor.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+
+[CustomEditor(typeof(Neuron.NeuronTracker))]
+public class NeuronTrackerInstanceEditor : Editor 
+{
+    SerializedProperty addressField;
+    SerializedProperty tcpPortField;
+    SerializedProperty udpPortField;
+    SerializedProperty tcpOrUdpField;
+    SerializedProperty rigidbodyField;
+    //void OnEnable()
+    //{
+    //    // Setup the SerializedProperties.
+    //    transformsField = serializedObject.FindProperty("transforms");
+    //}
+
+    public override void OnInspectorGUI()
+    {
+        Neuron.NeuronTracker script = (Neuron.NeuronTracker)target;
+
+        if (addressField == null)
+        {
+            addressField = serializedObject.FindProperty("address");
+            tcpPortField = serializedObject.FindProperty("portTcp");
+            udpPortField = serializedObject.FindProperty("portUdp");
+            tcpOrUdpField = serializedObject.FindProperty("socketType");
+            rigidbodyField = serializedObject.FindProperty("deviceName");
+        }
+        EditorGUILayout.PropertyField(addressField);
+        if (script.socketType == Neuron.NeuronEnums.SocketType.TCP)
+            EditorGUILayout.PropertyField(tcpPortField);
+        else if (script.socketType == Neuron.NeuronEnums.SocketType.UDP)
+            EditorGUILayout.PropertyField(udpPortField);
+
+        EditorGUILayout.PropertyField(tcpOrUdpField);
+        EditorGUILayout.PropertyField(rigidbodyField);
+        serializedObject.ApplyModifiedProperties();
+        //DrawDefaultInspector();
+        serializedObject.ApplyModifiedProperties();
+
+    }
+}
